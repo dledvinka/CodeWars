c4aa5ffbecd4b688c97534e0d0fc3b85b0941375
David Ledvinka <ledvinka.david@gmail.com>
2021-10-16 22:08:50 +0200

    abstract factory

---
+++ Sources/CodeWars/CodeWars.ConsoleApp/AbstractFactory.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace CodeWars.ConsoleApp
+{
+    public class AnimalWorld
+    {
+        private readonly Herbivore _herbivore;
+        private readonly Carnivore _carnivore;
+
+        public AnimalWorld(ContinentFactory continentFactory)
+        {
+            _herbivore = continentFactory.CreateHerbivore();
+            _carnivore = continentFactory.CreateCarnivore();
+        }
+        
+        public void RunFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+
+    public class AfricaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Wildebeest();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
+        }
+    }
+ 
+    /// <summary>
+    /// The 'ConcreteFactory2' class
+    /// </summary>
+    public class AmericaFactory : ContinentFactory
+    {
+        public override Herbivore CreateHerbivore()
+        {
+            return new Bison();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+    }
+ 
+    /// <summary>
+    /// The 'AbstractProductA' abstract class
+    /// </summary>
+    public abstract class Herbivore
+    {
+    }
+ 
+    /// <summary>
+    /// The 'AbstractProductB' abstract class
+    /// </summary>
+    public abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+ 
+    /// <summary>
+    /// The 'ProductA1' class
+    /// </summary>
+    public class Wildebeest : Herbivore
+    {
+    }
+ 
+    /// <summary>
+    /// The 'ProductB1' class
+    /// </summary>
+    public class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Wildebeest
+            Console.WriteLine(this.GetType().Name +
+                              " eats " + h.GetType().Name);
+        }
+    }
+ 
+    /// <summary>
+    /// The 'ProductA2' class
+    /// </summary>
+    public class Bison : Herbivore
+    {
+    }
+ 
+    /// <summary>
+    /// The 'ProductB2' class
+    /// </summary>
+    public class Wolf : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Bison
+            Console.WriteLine(this.GetType().Name +
+                              " eats " + h.GetType().Name);
+        }
+    }
+
+    public abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+}
---
+++ Sources/CodeWars/CodeWars.Tests/AbstractFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars.ConsoleApp;
+using NUnit.Framework;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    public class AbstractFactoryTests
+    {
+        [Test]
+        public void Test()
+        {
+            var africaFactory = new AfricaFactory();
+            
+            var animalWorld = new AnimalWorld(africaFactory);
+            animalWorld.RunFoodChain();
+        }
+        
+        [Test]
+        public void TestAmerica()
+        {
+            var americaFactory = new AmericaFactory();
+            
+            var animalWorld = new AnimalWorld(americaFactory);
+            animalWorld.RunFoodChain();
+        }
+    }
+}
